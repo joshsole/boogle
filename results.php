630fd94516e6a85793762ec6488efff041fc556a
joshsole <Josh@Joshs-MBP.atcltd.atc.org.nz>
2017-02-27 16:56:50 +1300

    search data and nothing option added

--- results.php
+++ results.php
@@ -2,24 +2,23 @@
 
 $searchQuery = $_GET['q'];
 
-$data = array
-(
-  array(
-    'text'=>'Apple',
-    'url'=> 'http://apple.com'),
-  array(
-    'text'=>'Bakers Delight',
-    'url'=>'http://bakersdelight.co.nz'
-    )
+$data = require 'includes/data.php';
 
-  );
-// die(var_dump($data));
-// foreach ($data as $term):
-  ?>
-  <!-- <a href="<?= $term['url'] ?>"><?= $term['text'] ?></a><br> -->
-  <?php
-// endforeach;
-// die();
+function filterResults($searchQuery, $data) {
+        $matches = [];
+ 
+        foreach ($data as $key => $value) {
+          if (strpos(strtolower($value['text']), strtolower($searchQuery)) !== false) {
+            $matches[] = $value;
+          }
+        }
+        return $matches;
+    }
+ 
+
+$terms = filterResults($searchQuery, $data);
+
+
 
   require 'partials/header.php';
   require 'partials/navigation.php';
@@ -35,14 +34,39 @@ $data = array
 
         <ul class="list-group">
           <?php
-          foreach ($data as $term):
+                  if (empty($terms)):
+                  ?>
+                  <li class="list-group-item notification-bar-fail m-b-1">
+
+
+                    <div class="notification-bar-details">
+                      <h3 class="notification-bar-title">
+                        Nothing was found :(<br>
+                      </h3>
+                      <p>
+                        <strong>Suggestions:</strong>
+                        <ul>
+                          <li>Make sure that all words are spelled correctly.</li>
+                          <li>Try different keywords.</li>
+                          <li>Try more general keywords.</li>
+                        </ul>
+
+                      </p>
+                    </div>
+                  </li>
+                  <!-- End of single result -->
+                  <?php
+                  else:
+          // the foreach code should be here..... don't forget to close the if statement below!
+
+          foreach ($terms as $term):
             ?>
           <!-- Single Result -->
           <li class="list-group-item notification-bar-fail m-b-1">
             <div class="notification-bar-details">
-              
-              <a href="$url" class="notification-bar-title">
+              <a href="<?= $term['url'] ?>" class="notification-bar-title" target=_blank>
                  <?= $term['text'] ?>
+
               </a>
               <span class="text-muted"><?= $term['url'] ?></span>
             </div>
@@ -50,7 +74,9 @@ $data = array
           <!-- End of single result -->
           <?php
           endforeach;
+          endif;
           ?>
+
           <!-- End of single result -->
         </ul>
       </div>

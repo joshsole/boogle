f30984ec7c1accf7872497860919fb21b675ba55
joshsole <Josh@Joshs-MBP.atcltd.atc.org.nz>
2017-02-27 16:17:14 +1300

    data added

--- results.php
+++ results.php
@@ -8,80 +8,56 @@ $data = array
     'text'=>'Apple',
     'url'=> 'http://apple.com'),
   array(
-'text'=>'Bakers Delight',
-'url'=>'http://bakersdelight.co.nz'
+    'text'=>'Bakers Delight',
+    'url'=>'http://bakersdelight.co.nz'
     )
 
   );
 // die(var_dump($data));
-foreach ($data as $term) {
-  echo '<a href="' . $term['url'] . '">' . $term['text'] . '</a><br>';
-}
+// foreach ($data as $term):
+  ?>
+  <!-- <a href="<?= $term['url'] ?>"><?= $term['text'] ?></a><br> -->
+  <?php
+// endforeach;
+// die();
 
-require 'partials/header.php';
-require 'partials/navigation.php';
-?>
+  require 'partials/header.php';
+  require 'partials/navigation.php';
+  ?>
 
 
-<!-- Content -->
-<div class="container">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <h4>Search results for <strong><?= $searchQuery;?></strong></h4>
-      <!-- Displayed results -->
-      <ul class="list-group">
-        <!-- Single Result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
+  <!-- Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h4>Search results for <strong><?= $searchQuery;?></strong></h4>
+        <!-- Displayed results -->
+
+        <ul class="list-group">
+          <?php
+          foreach ($data as $term):
+            ?>
+          <!-- Single Result -->
+          <li class="list-group-item notification-bar-fail m-b-1">
+            <div class="notification-bar-details">
+              
+              <a href="$url" class="notification-bar-title">
+                 <?= $term['text'] ?>
+              </a>
+              <span class="text-muted"><?= $term['url'] ?></span>
             </div>
-          </div>
-
-          <div class="notification-bar-details">
-            <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
-
-          <div class="notification-bar-details">
-            <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-        <li class="list-group-item notification-bar-fail m-b-1">
-          <div href="#" class="notification-bar-icon">
-            <div>
-              <i></i>
-            </div>
-          </div>
-
-          <div class="notification-bar-details">
-            <a href="$url" class="notification-bar-title">
-              $searchQuery['text']
-            </a>
-            <span class="text-muted">$searchQuery['url']</span>
-          </div>
-        </li>
-        <!-- End of single result -->
-      </ul>
+          </li>
+          <!-- End of single result -->
+          <?php
+          endforeach;
+          ?>
+          <!-- End of single result -->
+        </ul>
+      </div>
     </div>
   </div>
-</div>
 
-<?php 
-require 'partials/search-footer.php';
-require 'partials/footer.php';
-?>
+  <?php 
+  require 'partials/search-footer.php';
+  require 'partials/footer.php';
+  ?>
